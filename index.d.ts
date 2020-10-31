06b37201183cee9e68667a628deae2eddda4aa0e
Alex Wieland <wieland.alex1996@gmail.com>
2020-10-31 21:59:39 +0100

    fix typescript declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,35 @@
 declare module 'objection-js-soft-delete' {
-import { Model, QueryBuilder } from 'objection';
+    import { Model, QueryBuilder } from 'objection';
 
-class SDQueryBuilder<M extends Model, R = M[]> extends QueryBuilder<M, R> {
-    ArrayQueryBuilderType!: SDQueryBuilder<M>;
+    class SDQueryBuilder<M extends Model, R = M[]> extends QueryBuilder<M, R> {
+        ArrayQueryBuilderType: SDQueryBuilder<M>;
 
-    NumberQueryBuilderType!: SDQueryBuilder<M, number>;
+        NumberQueryBuilderType: SDQueryBuilder<M, number>;
 
-    delete(): this['NumberQueryBuilderType'];
-    hardDelete(): this['NumberQueryBuilderType'];
-    undelete(): this['NumberQueryBuilderType'];
-    whereDeleted(): this['ArrayQueryBuilderType'];
-    whereNotDeleted(): this['ArrayQueryBuilderType'];
+        delete(): this['NumberQueryBuilderType'];
+        hardDelete(): this['NumberQueryBuilderType'];
+        undelete(): this['NumberQueryBuilderType'];
+        whereDeleted(): this['ArrayQueryBuilderType'];
+        whereNotDeleted(): this['ArrayQueryBuilderType'];
+    }
+
+    interface SDInstance<T extends typeof Model> {
+        QueryBuilderType: SDQueryBuilder<this & T['prototype']>;
+    }
+
+    interface SDStatic<T extends typeof Model> {
+        QueryBuilder: typeof SDQueryBuilder;
+        isSoftDelete: boolean;
+        namedFilters(): object;
+
+        new (): SDInstance<T> & T['prototype'];
+    }
+
+    export default function softDelete<T extends typeof Model>(
+        options?: Partial<{
+            columnName: string;
+            deletedValue: Date | true | number;
+            notDeletedValue: false | null;
+        }>
+    ): (model: T) => SDStatic<T> & Omit<T, 'new'> & T['prototype'];
 }
