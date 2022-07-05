f69559dd6b442560f995441a8567ea54550fc3fb
Alex Wieland <alex.wieland@openforce.com>
2022-07-05 21:21:08 +0200

    enable ts declaration

--- dist/index.js
+++ dist/index.js
@@ -1,69 +1,70 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const objection_1 = require("objection");
 function softDelete(pluginOptions) {
     const options = Object.assign({ columnName: 'deleted_at', deletedValue: new Date(), notDeletedValue: null }, pluginOptions);
-    return (Model) => {
-        class SDQueryBuilder extends Model.QueryBuilder {
-            // override the normal delete function with one that patches the row's "deleted" column
-            delete() {
-                this.context({
-                    softDelete: true,
-                });
-                const patch = {};
-                patch[options.columnName] = options.deletedValue;
-                return this.patch(patch);
-            }
-            // provide a way to actually delete the row if necessary
-            hardDelete() {
-                return super.delete();
-            }
-            // provide a way to undo the delete
-            undelete() {
-                this.context({
-                    undelete: true,
-                });
-                const patch = {};
-                patch[options.columnName] = options.notDeletedValue;
-                return this.patch(patch);
-            }
-            // provide a way to filter to ONLY deleted records without having to remember the column name
-            whereDeleted() {
-                const modelClass = this.modelClass();
-                const tableRef = this.tableRefFor(modelClass);
-                // this if is for backwards compatibility, to protect those that used a nullable `deleted` field
-                if (options.deletedValue === true) {
-                    return this.where(`${tableRef}.${options.columnName}`, options.deletedValue);
-                }
-                // qualify the column name
-                return this.whereNot(`${tableRef}.${options.columnName}`, options.notDeletedValue);
-            }
-            // provide a way to filter out deleted records without having to remember the column name
-            whereNotDeleted() {
-                const modelClass = this.modelClass();
-                const tableRef = this.tableRefFor(modelClass);
-                // qualify the column name
-                return this.where(`${tableRef}.${options.columnName}`, options.notDeletedValue);
-            }
+    class SDQueryBuilder extends objection_1.QueryBuilder {
+        // override the normal delete function with one that patches the row's "deleted" column
+        delete() {
+            this.context({
+                softDelete: true,
+            });
+            const patch = {};
+            patch[options.columnName] = options.deletedValue;
+            return this.patch(patch);
         }
-        return class extends Model {
-            static get QueryBuilder() {
-                return SDQueryBuilder;
+        // provide a way to actually delete the row if necessary
+        hardDelete() {
+            return super.delete();
+        }
+        // provide a way to undo the delete
+        undelete() {
+            this.context({
+                undelete: true,
+            });
+            const patch = {};
+            patch[options.columnName] = options.notDeletedValue;
+            return this.patch(patch);
+        }
+        // provide a way to filter to ONLY deleted records without having to remember the column name
+        whereDeleted() {
+            const modelClass = this.modelClass();
+            const tableRef = this.tableRefFor(modelClass);
+            // this if is for backwards compatibility, to protect those that used a nullable `deleted` field
+            if (options.deletedValue === true) {
+                return this.where(`${tableRef}.${options.columnName}`, options.deletedValue);
             }
-            static get modifiers() {
-                return {
-                    notDeleted(builder) {
-                        builder.whereNotDeleted();
-                    },
-                    deleted(builder) {
-                        builder.whereDeleted();
-                    },
-                };
-            }
-            static get isSoftDelete() {
-                return true;
-            }
-        };
+            // qualify the column name
+            return this.whereNot(`${tableRef}.${options.columnName}`, options.notDeletedValue);
+        }
+        // provide a way to filter out deleted records without having to remember the column name
+        whereNotDeleted() {
+            const modelClass = this.modelClass();
+            const tableRef = this.tableRefFor(modelClass);
+            // qualify the column name
+            return this.where(`${tableRef}.${options.columnName}`, options.notDeletedValue);
+        }
+    }
+    return function (Base) {
+        var _a;
+        return _a = class extends Base {
+                static get modifiers() {
+                    return {
+                        notDeleted(builder) {
+                            builder.whereNotDeleted();
+                        },
+                        deleted(builder) {
+                            builder.whereDeleted();
+                        },
+                    };
+                }
+                static get isSoftDelete() {
+                    return true;
+                }
+            },
+            _a.options = options,
+            _a.QueryBuilder = SDQueryBuilder,
+            _a;
     };
 }
-;
 exports.default = softDelete;
--- src/index.ts
+++ src/index.ts
@@ -1,129 +1,132 @@
 import {
-  Model,
-  QueryBuilder,
-  Page,
-  PartialModelObject,
+    Model,
+    QueryBuilder,
+    Page,
+    PartialModelObject,
 } from 'objection';
 
 interface PluginOptions {
-  columnName: string;
-  deletedValue: Date | boolean | number;
-  notDeletedValue: boolean | null;
+    columnName: string;
+    deletedValue: Date | boolean | number;
+    notDeletedValue: boolean | null;
 }
 
 function softDelete(pluginOptions?: Partial<PluginOptions>) {
-  const options: PluginOptions = {
-    columnName: 'deleted_at',
-    deletedValue: new Date(),
-    notDeletedValue: null,
-    ...pluginOptions,
-  };
+    const options: PluginOptions = {
+        columnName: 'deleted_at',
+        deletedValue: new Date(),
+        notDeletedValue: null,
+        ...pluginOptions,
+    };
 
-  type SDModelConstructor = new (...input: any[]) => Model;
-  type SDMixin<T extends SDModelConstructor> = SDModel<T> & T;
+    type SDModelConstructor = new (...input: any[]) => Model;
+    type SDMixin<T extends SDModelConstructor> = SDModel<T> & T;
 
-  interface SDModelInstance<T extends SDModelConstructor> {
-    /* MyQueryBuilder<this, this[]> would be optimal, but not possible since `this`
-     * doesn't extend Model. Not sure what this affects either.
-     */
-    QueryBuilderType: SDQueryBuilder<this & InstanceType<T>, this[]>;
-  }
-
-  class SDQueryBuilder<M extends Model, R = M[]> extends QueryBuilder<M, R> {
-    ArrayQueryBuilderType!: SDQueryBuilder<M, M[]>;
-    SingleQueryBuilderType!: SDQueryBuilder<M, M>;
-    NumberQueryBuilderType!: SDQueryBuilder<M, number>;
-    PageQueryBuilderType!: SDQueryBuilder<M, Page<M>>;
-
-    // override the normal delete function with one that patches the row's "deleted" column
-    delete(): this['NumberQueryBuilderType'] {
-      this.context({
-        softDelete: true,
-      });
-      const patch: PartialModelObject<M> = {};
-      patch[options.columnName] = options.deletedValue;
-
-      return this.patch(patch);
+    interface SDModel<T extends SDModelConstructor> {
+        options: PluginOptions;
+        isSoftDelete: boolean;
+        QueryBuilder: typeof SDQueryBuilder;
+        new(...args: any[]): SDModelInstance<T> & T;
     }
 
-    // provide a way to actually delete the row if necessary
-    hardDelete() {
-      return super.delete();
+
+    interface SDModelInstance<T extends SDModelConstructor> {
+        /* MyQueryBuilder<this, this[]> would be optimal, but not possible since `this`
+         * doesn't extend Model. Not sure what this affects either.
+         */
+        // @ts-ignore
+        QueryBuilderType: SDQueryBuilder<this & InstanceType<T>, this[]>;
     }
 
-    // provide a way to undo the delete
-    undelete() {
-      this.context({
-        undelete: true,
-      });
+    class SDQueryBuilder<M extends Model, R = M[]> extends QueryBuilder<M, R> {
+        ArrayQueryBuilderType!: SDQueryBuilder<M, M[]>;
+        SingleQueryBuilderType!: SDQueryBuilder<M, M>;
+        NumberQueryBuilderType!: SDQueryBuilder<M, number>;
+        PageQueryBuilderType!: SDQueryBuilder<M, Page<M>>;
 
-      const patch: PartialModelObject<M> = {};
-      patch[options.columnName] = options.notDeletedValue;
+        // override the normal delete function with one that patches the row's "deleted" column
+        delete(): this['NumberQueryBuilderType'] {
+            this.context({
+                softDelete: true,
+            });
+            const patch: Record<string, unknown> = {};
 
-      return this.patch(patch);
+            patch[options.columnName] = options.deletedValue;
+
+            return this.patch(patch as PartialModelObject<M>);
+        }
+
+        // provide a way to actually delete the row if necessary
+        hardDelete() {
+            return super.delete();
+        }
+
+        // provide a way to undo the delete
+        undelete() {
+            this.context({
+                undelete: true,
+            });
+
+            const patch: Record<string, unknown> = {};
+            patch[options.columnName] = options.notDeletedValue;
+
+            return this.patch(patch as PartialModelObject<M>);
+        }
+
+        // provide a way to filter to ONLY deleted records without having to remember the column name
+        whereDeleted() {
+            const modelClass = this.modelClass();
+            const tableRef = this.tableRefFor(modelClass as any);
+
+            // this if is for backwards compatibility, to protect those that used a nullable `deleted` field
+            if (options.deletedValue === true) {
+                return this.where(
+                    `${tableRef}.${options.columnName}`,
+                    options.deletedValue
+                );
+            }
+            // qualify the column name
+            return this.whereNot(
+                `${tableRef}.${options.columnName}`,
+                options.notDeletedValue
+            );
+        }
+
+        // provide a way to filter out deleted records without having to remember the column name
+        whereNotDeleted() {
+            const modelClass = this.modelClass();
+            const tableRef = this.tableRefFor(modelClass as any);
+
+            // qualify the column name
+            return this.where(
+                `${tableRef}.${options.columnName}`,
+                options.notDeletedValue
+            );
+        }
     }
 
-    // provide a way to filter to ONLY deleted records without having to remember the column name
-    whereDeleted() {
-      const modelClass = this.modelClass();
-      const tableRef = this.tableRefFor(modelClass as any);
+    return function <T extends SDModelConstructor>(Base: T) {
+        return class extends Base {
+            public static options = options;
+            static QueryBuilder = SDQueryBuilder;
+            QueryBuilderType!: SDQueryBuilder<this, this[]>;
 
-      // this if is for backwards compatibility, to protect those that used a nullable `deleted` field
-      if (options.deletedValue === true) {
-        return this.where(
-          `${tableRef}.${options.columnName}`,
-          options.deletedValue
-        );
-      }
-      // qualify the column name
-      return this.whereNot(
-        `${tableRef}.${options.columnName}`,
-        options.notDeletedValue
-      );
-    }
+            static get modifiers() {
+                return {
+                    notDeleted(builder: SDQueryBuilder<Model, Model[]>) {
+                        builder.whereNotDeleted();
+                    },
+                    deleted(builder: SDQueryBuilder<Model, Model[]>) {
+                        builder.whereDeleted();
+                    },
+                };
+            }
 
-    // provide a way to filter out deleted records without having to remember the column name
-    whereNotDeleted() {
-      const modelClass = this.modelClass();
-      const tableRef = this.tableRefFor(modelClass as any);
-
-      // qualify the column name
-      return this.where(
-        `${tableRef}.${options.columnName}`,
-        options.notDeletedValue
-      );
-    }
-  }
-
-  interface SDModel<T extends SDModelConstructor> {
-    options: PluginOptions;
-    isSoftDelete: boolean;
-    QueryBuilder: typeof SDQueryBuilder;
-    new (...args: any[]): SDModelInstance<T> & T;
-  }
-
-  return function <T extends SDModelConstructor>(Base: T) {
-    return class extends Base {
-      public static options = options;
-      static QueryBuilder = SDQueryBuilder;
-      QueryBuilderType!: SDQueryBuilder<this, this[]>;
-
-      static get modifiers() {
-        return {
-          notDeleted(builder: SDQueryBuilder<Model, Model[]>) {
-            builder.whereNotDeleted();
-          },
-          deleted(builder: SDQueryBuilder<Model, Model[]>) {
-            builder.whereDeleted();
-          },
-        };
-      }
-
-      static get isSoftDelete() {
-        return true;
-      }
-    } as unknown as SDMixin<T>;
-  };
+            static get isSoftDelete() {
+                return true;
+            }
+        } as unknown as SDMixin<T>;
+    };
 }
 
 export default softDelete;
