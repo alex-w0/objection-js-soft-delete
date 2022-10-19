1aef6ab02d4a7c46916e8d061a5907a1015b9b26
Alex Wieland <alex.wieland@openforce.com>
2022-10-19 20:20:34 +0200

    fix: update type declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,35 @@
 declare module 'objection-js-soft-delete' {
     import { Model, QueryBuilder } from 'objection';
-
-    class SDQueryBuilder<M extends Model, R = M[]> extends QueryBuilder<M, R> {
-        ArrayQueryBuilderType: SDQueryBuilder<M>;
-
-        NumberQueryBuilderType: SDQueryBuilder<M, number>;
-
-        delete(): this['NumberQueryBuilderType'];
-        hardDelete(): this['NumberQueryBuilderType'];
-        undelete(): this['NumberQueryBuilderType'];
-        whereDeleted(): this['ArrayQueryBuilderType'];
-        whereNotDeleted(): this['ArrayQueryBuilderType'];
+  
+    class SoftDeleteQueryBuilder<M extends Model, R = M[]> extends QueryBuilder<M, R> {
+      ArrayQueryBuilderType: SoftDeleteQueryBuilder<M>;
+  
+      NumberQueryBuilderType: SoftDeleteQueryBuilder<M, number>;
+  
+      delete(): this['NumberQueryBuilderType'];
+      hardDelete(): this['NumberQueryBuilderType'];
+      undelete(): this['NumberQueryBuilderType'];
+      whereDeleted(): this['ArrayQueryBuilderType'];
+      whereNotDeleted(): this['ArrayQueryBuilderType'];
     }
-
-    interface SDInstance<T extends typeof Model> {
-        QueryBuilderType: SDQueryBuilder<this & T['prototype']>;
+  
+    interface SoftDeleteInstance<T extends typeof Model> {
+      //@ts-ignore
+      QueryBuilderType: SoftDeleteQueryBuilder<this, this[]>;
     }
-
-    interface SDStatic<T extends typeof Model> {
-        QueryBuilder: typeof SDQueryBuilder;
-        isSoftDelete: boolean;
-        namedFilters(): object;
-
-        new (): SDInstance<T> & T['prototype'];
+  
+    interface SoftDeleteStatic<T extends typeof Model> {
+      QueryBuilder: typeof SoftDeleteQueryBuilder;
+      isSoftDelete: boolean;
+      namedFilters(): object;
+      new (): SoftDeleteInstance<T> & T['prototype'];
     }
-
+  
     export default function softDelete<T extends typeof Model>(
-        options?: Partial<{
-            columnName: string;
-            deletedValue: Date | boolean | number;
-            notDeletedValue: boolean | null;
-        }>
-    ): (model: T) => SDStatic<T> & Omit<T, 'new'> & T['prototype'];
-}
+      options?: Partial<{
+        columnName: string;
+        deletedValue: Date | boolean | number;
+        notDeletedValue: boolean | null;
+      }>,
+    ): (model: T) => Omit<T, 'new'> & SoftDeleteStatic<T>;
+  }
